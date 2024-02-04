4bd52df154134dcc06976f3965516b0e990bab10
Diogo Igarassu <euamofisica2006@hotmail.com>
2024-02-03 23:33:52 -0300

    melhorias nas config de email e resultados das analises

--- lab03.py
+++ lab03.py
@@ -1,10 +1,20 @@
 import smtplib
 import cores as cor
 
+def obter_credenciais():
+    with open('config.txt', 'r') as file:
+        lines = file.readlines()
+        credenciais = {}
+        for line in lines:
+            key, value = line.strip().split('=')
+            credenciais[key] = value
+        return credenciais
+
 def enviar_email(assunto, mensagem):
-    remetente = 'seuemail@gmail.com'
-    senha = 'seua senha'
-    destinatario = 'seuemail@gmail.com'
+    credenciais = obter_credenciais()
+    remetente = credenciais['email']
+    senha = credenciais['senha']
+    destinatario = credenciais['destinatario']
 
     corpo = f"Subject: {assunto}\n\n{mensagem}"
     corpo = corpo.encode('utf-8')
@@ -17,4 +27,4 @@ def enviar_email(assunto, mensagem):
 
         print(cor.FIM, cor.BRA, cor.BPRE, "\nE-mail enviado para informar o término do processo..\n", cor.FIM)
 
-#enviar_email("teste de email ML", "Teste para informar fins dos cálculos")
+enviar_email("teste de email ML", "Teste para informar fins dos cálculos")
